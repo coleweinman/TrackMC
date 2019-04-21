b8c1c1451ec595932033db94f59d227db7fb23b6
Unknown <coleweinman@gmail.com>
2019-04-21 09:58:02 -0500

    Updated layout

--- script.js
+++ script.js
@@ -41,20 +41,20 @@ function updateIP(ip) {
             }
             var element = document.getElementById(ip);
             if(element != null) {
-                element.childNodes[1].innerHTML = "<b>Players Online: </b>"+res.playerCount+"/"+res.playerMax;
-                if(res.playerList.toString() == "n/a" || res.playerList.toString() == "") {
-                  element.childNodes[3].innerHTML = "";
-                } else {
-                  element.childNodes[3].innerHTML = "<b>Players: </b>"+res.playerList.toString();
-                }
-                element.childNodes[5].innerHTML = "<b>Version: </b>"+res.version+" "+res.protocal;
-                element.childNodes[7].innerHTML = "<b>MOTD: </b>"+motd;
-                element.setAttribute('style','');
+              element.childNodes[1].innerHTML = "<b>IP: </b>"+ip;
+              element.childNodes[3].innerHTML = "<b>MOTD: </b>"+motd;
+              element.childNodes[5].innerHTML = "<b>Players Online: </b>"+res.playerCount+"/"+res.playerMax;
+              if(res.playerList.toString() == "n/a" || res.playerList.toString() == "") {
+                element.childNodes[7].innerHTML = "";
+              } else {
+                element.childNodes[7].innerHTML = "<b>Players: </b>"+res.playerList.toString();
+              }
+              element.childNodes[9].innerHTML = "<b>Version: </b>"+res.version+" "+res.protocal;
+              element.setAttribute('style','width: fit-content;');
             }
         }
     };
     xhttp.open("GET", "https://redtech-trackmc.appspot.com/?ip="+ip, true);
-    //xhttp.setRequestHeader('Access-Control-Allow-Headers', '*');
     xhttp.send();
 
 }
